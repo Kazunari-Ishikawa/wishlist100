4e26000d225230697ec251c4167c4da579de3003
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-03-27 08:21:38 +0900

    Modify controller name

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,23 @@
 Auth::routes();
 
 // TOPページ
-Route::get('/', 'WishController@index')->name('index');
+Route::get('/', 'WishesController@index')->name('index');
 // マイページ、やりたいこと表示
-Route::get('/mypage', 'WishController@show')->name('wish.show');
+Route::get('/mypage', 'WishesController@mypage')->name('wish.mypage');
 // マイページ、やったこと表示
-Route::get('/mypage/done', 'WishController@done_show')->name('wish.done_show');
+Route::get('/mypage/done', 'WishesController@done_show')->name('wish.done_show');
 // 新規作成表示
-Route::get('/mypage/new', 'WishController@new')->name('wish.new');
+Route::get('/mypage/new', 'WishesController@new')->name('wish.new');
 // やりたいこと編集表示
-Route::get('/mypage/{id}/edit', 'WishController@edit')->name('wish.edit');
+Route::get('/mypage/{id}/edit', 'WishesController@edit')->name('wish.edit');
 // アカウント設定表示
-Route::get('/setting/account', 'SettingController@account')->name('setting.account');
+Route::get('/setting/account', 'SettingsController@account')->name('setting.account');
 // リスト設定表示
-Route::get('/setting/list', 'SettingController@list')->name('setting.list');
+Route::get('/setting/list', 'SettingsController@list')->name('setting.list');
 
 // 新規作成機能
-Route::post('/mypage', 'WishController@create')->name('wish.create');
+Route::post('/mypage', 'WishesController@create')->name('wish.create');
 // 編集機能
-Route::post('/mypage/{id}', 'WishController@update')->name('wish.update');
+Route::post('/mypage/{id}', 'WishesController@update')->name('wish.update');
 // 削除機能
-Route::post('/mypage/{id}/delete', 'WishController@destroy')->name('wish.destroy');
+Route::post('/mypage/{id}/delete', 'WishesController@destroy')->name('wish.destroy');
